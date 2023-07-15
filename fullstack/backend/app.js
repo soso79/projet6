27bc0b7c13ed1e379c22d17af5a197f5789007d0
sofiane bahri <bahrisofiane79@gmail.com>
2023-07-15 18:18:45 +0200

    routes importées

--- fullstack/backend/app.js
+++ fullstack/backend/app.js
@@ -6,13 +6,18 @@ const mongoose = require('mongoose');
 
 const Sauce = require('./models/Sauce');
 
+const HotTakesRoutes = require('./routes/HotTakes');
+
+app.use('/api/HotTakes', HotTakesRoutes);
+
+
 mongoose.connect('mongodb+srv://soso:Manimal@cluster0.r62xbdt.mongodb.net/',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+  
 
 app.use(express.json());
 
@@ -24,44 +29,7 @@ app.use((req, res, next) => {
   });
   
 
-  app.post('/api/HotTakes', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-
-  app.put('/api/HotTakes/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
   
-  app.delete('/api/HotTakes/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-
-  app.get('/api/HotTakes/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-
-
-app.get('/api/HotTakes', (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
     
   
 
