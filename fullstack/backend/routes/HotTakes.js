d550038ea63aca31929c0f65cdaad2416b00a008
sofiane bahri <bahrisofiane79@gmail.com>
2023-07-16 16:44:16 +0200

    router et controller fini

--- fullstack/backend/routes/HotTakes.js
+++ fullstack/backend/routes/HotTakes.js
@@ -1,100 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Sauce = require('../models/Sauce');
+const HotTakesCtrl = require('../controllers/HotTakes');
 
-router.post('/', (req, res, next) => {
-  const sauce = new Sauce({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  sauce.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-router.get('/:id', (req, res, next) => {
-  Sauce.findOne({
-    _id: req.params.id
-  }).then(
-    (sauce) => {
-      res.status(200).json(sauce);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
-});
-
-router.put('/:id', (req, res, next) => {
-  const sauce = new Sauce({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Sauce.updateOne({_id: req.params.id}, sauce).then(
-    () => {
-      res.status(201).json({
-        message: 'Sauce updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-router.delete('/:id', (req, res, next) => {
-  Sauce.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-router.get('/' +
-  '', (req, res, next) => {
-  Sauce.find().then(
-    (things) => {
-      res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+router.get('/', HotTakesCtrl.getAllSauce);
+router.post('/', HotTakesCtrl.createSauce);
+router.get('/:id', HotTakesCtrl.getOneSauce);
+router.put('/:id', HotTakesCtrl.modifySauce);
+router.delete('/:id', HotTakesCtrl.deleteSauce);
 
 module.exports = router;
