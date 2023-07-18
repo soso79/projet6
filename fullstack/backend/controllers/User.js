bc1dd113727f5fe45930eb5666ac056e45f1d696
sofiane bahri <bahrisofiane79@gmail.com>
2023-07-18 16:01:09 +0200

    creation dossier middleware

--- fullstack/backend/controllers/User.js
+++ fullstack/backend/controllers/User.js
@@ -20,16 +20,21 @@ exports.signup = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ message: 'Utilisateur non trouvé !'});
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        return res.status(401).json({ message: 'Mot de passe incorrect !' });
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expires: '24h' }
+
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
